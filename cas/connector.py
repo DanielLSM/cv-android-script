88dc2cab648b8e7f7deca2aca833bb635a93947e
Daniel <daniellsmarta@gmail.com>
2020-02-27 18:37:45 +0100

    initial commit

---
+++ cas/connector.py
@@ -0,0 +1,33 @@
+import cv2
+import vlc
+import requests
+import numpy as np 
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+
+
+    p = vlc.MediaPlayer("http://130.229.150.214:8080/audio.wav")
+    p.play()
+    while True:
+        url = "http://130.229.150.214:8080/shot.jpg"
+
+        img_shot =  requests.get(url)
+        img_array = np.array(bytearray(img_shot.content), dtype=np.uint8)
+        img = cv2.imdecode(img_array, -1)
+
+        cv2.imshow("AndroidCam", img)
+
+        if cv2.waitKey(1)==27:
+            break
+
+
